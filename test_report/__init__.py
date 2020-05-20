5402f92c37703a674fde6d90822f79df24201696
Davini <edoardo.davini@gmail.com>
2020-05-20 19:21:44 +0200

    Refactor of report generation with names and description of steps and with better design concept

--- test_report/__init__.py
+++ test_report/__init__.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 # http calls with string building:
@@ -13,48 +12,75 @@ def build_report(responses, suite, ROOT_RESULT):
 
         print('Building Markdown Summary Report for {0}'.format(suite.get('name')))
         destination = ROOT_RESULT + clean_filename + '_summary.md'
-        build_markdown_summary(destination, responses)
+        build_markdown(destination, responses, suite)
 
 
-def build_markdown_summary(destination, responses):
-
+def build_markdown(destination, responses, suite):
     report = '''
-# API Report results
-    '''
-    # TODO: HANDLE MULTIPLE ASSERTION STEPS
-    assertions = list(filter(lambda x: x.get('type', 'ASSERT') == 'ASSERT', responses))[0]
+# Report {name}
+> {desc}
+'''.format(name=suite.get('name', 'Unnamed Suite'), desc=suite.get('description', ''))
 
-    percentage = round(assertions['summary']['passed'] * 100 / assertions['summary']['total'], 2)
+    assertions = list(filter(lambda x: x.get('type', 'ASSERT') == 'ASSERT', responses))
+
+    report += build_markdown_summary(assertions)
+
+    for assertion in assertions:
+        report += build_markdown_detailed(assertion)
+
+    open(destination, 'w+').write(report)
+
+
+def build_markdown_summary(assertions):
+
+    report = ""
+
+    # stats
+    total_checks = 0
+    total_passed = 0
+    total_failed = 0
+
+    for assertion in assertions:
+        total_checks += assertion.get('summary', {}).get('total', 0)
+        total_passed += assertion.get('summary', {}).get('passed', 0)
+        total_failed += assertion.get('summary', {}).get('failed', 0)
+
+    percentage = round(total_passed * 100 / total_checks, 2)
 
     report += '''
 * Total Tests: {total}
 * Passed Tests: {passed}
 * Failed Tests: {failed}
 * Percentage: **{percentage}**  '''.format(
-        total=assertions['summary']['total'],
-        passed=assertions['summary']['passed'],
-        failed=assertions['summary']['failed'],
+        total=total_checks,
+        passed=total_passed,
+        failed=total_failed,
         percentage=str(percentage) + '%'
     )
 
-    for i in range(assertions['summary']['total']):
-        if i < assertions['summary']['passed']:
+    for i in range(total_checks):
+        if i < total_passed:
             report += ':green_square:'
         else:
             report += ':red_square:'
 
-    report += '''
+    return report
 
+
+def build_markdown_detailed(assertion):
+    report = '''
+
+**{name}**
 | Check | Filled Check | Result |
 | ------------ | --------- | ----- |
-'''
+'''.format(name=assertion.get('name'))
 
-    for result in assertions['results']:
+    for result in assertion['results']:
         report += '| {check} | {filled} | {result} | \n'.format(
             check=result['check'],
             filled=result['filled_check'],
             # result='&#x2714;' if result['result'] else '&#x2716;'
             result=':heavy_check_mark:' if result['result'] else ':x:'
         )
+    return report
 
-    open(destination, 'w+').write(report)
